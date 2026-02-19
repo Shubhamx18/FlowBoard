a56b8afc5a57290e79d350f02be2335f9bfcf85c
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-02-20 01:34:12 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,10 +1,22 @@
 def call() {
-    sh """
+
+    sh '''
+        echo "Starting OWASP Dependency Check..."
+
+        # Ensure data directory exists
+        mkdir -p /opt/dependency-check-data
+
+        # Run Dependency Check
         /opt/dependency-check-tool/bin/dependency-check.sh \
-        --project Luminary \
-        --scan . \
-        --format XML \
-        --out dependency-check-report \
-        --data /opt/dependency-check-data
-    """
+        --project "Luminary" \
+        --scan "." \
+        --format "XML" \
+        --out "dependency-check-report" \
+        --data "/opt/dependency-check-data" \
+        --disableAssembly \
+        --disableNodeAudit
+
+        echo "OWASP Dependency Check Completed"
+    '''
+
 }

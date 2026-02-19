9cc679fd70f8235ca22123e1b21853193b4b0326
shubham Sadashiv mali <149328245+Shubhamx18@users.noreply.github.com>
2026-02-20 03:33:30 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,9 @@
 def call(String imageName, String imageTag, String dockerHubUser) {
     sh """
+        sudo usermod -aG docker jenkins
+        sudo reboot
+        sudo su - jenkins
+        docker ps
         docker build -t ${dockerHubUser}/${imageName}:${imageTag} .
     """
 }
